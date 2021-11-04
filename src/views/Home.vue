a7bae45b2aef3bb41be14cd2e42b7865ee793e7f
Sebastien <sebastienleonce91@gmail.com>
2021-11-04 11:26:51 +0100

    remove useless file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,84 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>ToDo App</h1>
+    <form @submit.prevent="addTodo()">
+        <label>New ToDo </label>
+        <input
+            v-model="newTodo"
+            name="newTodo"
+            autocomplete="off"
+        >
+        <button>Add ToDo</button>
+    </form>
+    <h2>ToDo List</h2>
+    <ul>
+        <li
+            v-for="(todo, index) in todos"
+            :key="index"
+        >
+            <span
+                :class="{ done: todo.done }"
+                @click="doneTodo(todo)"
+            >{{ todo.content }}</span>
+            <button @click="removeTodo(index)">Remove</button>
+        </li>
+    </ul>
+    <h4 v-if="todos.length === 0">Empty list.</h4>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent, ref } from 'vue';
+
+interface Todo {
+  content: string,
+  done: boolean
+}
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld,
   },
+  setup () {
+    const newTodo = ref('');
+    const defaultData = JSON.stringify([{
+        done: false,
+        content: 'Write a blog post'
+    }]);
+
+    const todosData = JSON.parse(localStorage.getItem('todos') || defaultData);
+    const todos = ref(todosData);
+    
+    function addTodo () {
+        if (newTodo.value) {
+            todos.value.push({
+                done: false,
+                content: newTodo.value
+            });
+            newTodo.value = '';
+        }
+        saveData();
+    }
+    function doneTodo (todo : Todo) {
+        todo.done = !todo.done
+        saveData();
+    }
+    function removeTodo (index : number) {
+        todos.value.splice(index, 1);
+        saveData();
+    }
+    function saveData () {
+        const storageData = JSON.stringify(todos.value);
+        localStorage.setItem('todos', storageData);
+    }
+    return {
+        todos,
+        newTodo,
+        addTodo,
+        doneTodo,
+        removeTodo,
+        saveData
+    }
+  }
 });
 </script>
